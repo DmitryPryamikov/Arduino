2fc289e12175fbf120a8330ebc44e7529185ca0a
Dmitry Pryamikov <dimetrics@list.ru>
2018-02-11 20:19:17 +0600

    add comments

--- servo_ver_1.ino
+++ servo_ver_1.ino
@@ -4,36 +4,38 @@ Servo base_Servo;
 //хвататель
 Servo hvatatel;
 int servo_position=0;
-//
+//подключаем порты
 void setup() {
   // put your setup code here, to run once:
-  //опора
+  //опора к 10
   base_Servo.attach(10);
-  //хвататель
+  //хвататель к 13
   hvatatel.attach(13);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // поворачиваем платформу на 180 град
   for (servo_position=0; servo_position<=180;servo_position+=1)
   {
     base_Servo.write(servo_position);
+    //задержка на 0,1 сек
     delay(100);
   }
-  //
+  //Захватываем деталь
   for (servo_position=70; servo_position<=100;servo_position+=1)
   {
     hvatatel.write(servo_position);
+    //задержка на 0,1 сек
     delay(100);
     
   }
-  
+  //разворачиваем платформу
   for (servo_position=180; servo_position>=0;servo_position-=1)
   {
     base_Servo.write(servo_position);
     delay(100);
   }
-  //
+  // отпускаем деталь
    for (servo_position=100; servo_position>=70;servo_position-=1)
   {
   hvatatel.write(servo_position);
